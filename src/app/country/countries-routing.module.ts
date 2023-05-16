ef63688c4879be59eb6dcb5b8c0952fcb44c4678
PROD\rafael.palomino <rafael.palomino@alten.es>
2023-05-16 11:30:20 +0200

    Made Udemy course section 19 country app

---
+++ src/app/country/countries-routing.module.ts
@@ -0,0 +1,24 @@
+import { RouterModule, Routes } from '@angular/router';
+import { SelectorPageComponent } from './pages/selector-page/selector-page.component';
+import { NgModule } from '@angular/core';
+
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'selector',
+        component: SelectorPageComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'selector',
+      },
+    ],
+  },
+];
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CountriesRoutingModule {}
